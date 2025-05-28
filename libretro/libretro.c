b6ff8fc4dd0eff286591a359fa5f72ac3cdaa9e7
NullPopPo <sylph@reset.jp>
2025-05-28 15:28:37 +0000

    truly, FD type dip sw on means 2HD

--- libretro/libretro.c
+++ libretro/libretro.c
@@ -160,7 +160,7 @@ void retro_set_environment(retro_environment_t cb)
 
    struct retro_variable variables[] = {
       { "x1_resolute" , "Resolution; LOW|HIGH" },
-      { "x1_bootmedia" , "Boot media; 2HD|2D" },
+      { "x1_bootmedia" , "Boot media; 2D|2HD" },
       { "x1_romtype", "ROM type; X1|TURBO|TURBOZ" },
       { "x1_fps", "FPS; AUTO|60|30|20|15" },
       { "x1_dispsync", "Disp Vsync; OFF|ON" },
@@ -230,7 +230,7 @@ static void update_variables(void)
    }
 
    int resolute = get_booleanvar("x1_resolute", "LOW");
-   int bootmedia = get_booleanvar("x1_bootmedia", "2D");
+   int bootmedia = get_booleanvar("x1_bootmedia", "2HD");
 
    xmilcfg.DIP_SW = (resolute ? DIPSW_RESOLUTE : 0) | (bootmedia ? DIPSW_BOOTMEDIA : 0);
 
